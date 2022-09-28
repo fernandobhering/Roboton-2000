b5f3760e75dda5644b384249d010f2bb49734b65
unknown <fernandobhering0@gmail.com>
2022-09-28 14:36:35 -0300

    Bem-Vindo ao robotron 2000

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const robotron = document.querySelector("#robotron");
+
+robotron.addEventListener("click", function() {
+    console.log("Cliquei no robo.");
+});
+
+function dizoi() {
+    console.log("oi");
+    console.log("Bem-Vindo ao robotron 2000");
+}
+
+dizoi();
