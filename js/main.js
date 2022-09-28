cf85dad54af1237208f8d1a26dbd61b5c05916f3
unknown <fernandobhering0@gmail.com>
2022-09-28 15:42:01 -0300

    Atualização do robotron 2000

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
-const robotron = document.querySelector("#robotron");
+const subtrair = document.querySelector("#subtrair");
+const somar = document.querySelector("#somar");
+const braco = document.querySelector("#braco");
 
-robotron.addEventListener("click", function() {
-    console.log("Cliquei no robo.");
-});
+somar.addEventListener("click", () => {manipulaDados("somar")});
 
-function dizoi() {
-    console.log("oi");
-    console.log("Bem-Vindo ao robotron 2000");
+subtrair.addEventListener("click", () => {manipulaDados("subtrair")});
+
+function manipulaDados (operacao) {
+    if(operacao === "subtrair") {
+        braco.value = parseInt(braco.value) - 1;
+    } else {
+        braco.value = parseInt(braco.value) + 1;
+    }
 }
 
-dizoi();
